115b53df1cb0501cc87ccbac023d116a0fc4cc09
Zuzana Brýlová <zuzana.brylova@gmail.com>
2024-02-24 21:52:22 +0100

    Tridy final.

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ const product3 = {
   name: 'Anatoplavky',
   description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
+
+const prvniProdukt = document.querySelector("#product1");
+prvniProdukt.classList.add("border-primary");
+
+const tlacitko2 = document.querySelector("#product2 [type=button]");
+tlacitko2.classList.remove("btn-primary");
+
+const nazevKarty3 = document.querySelector("#product3 .card-title");
+nazevKarty3.classList.toggle("text-center");
